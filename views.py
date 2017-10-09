19101a1ffc4ffb65cc17a67ef9ed07a18bdefb29
Doug Wight <dcfwight@gmail.com>
2017-10-09 13:40:32 +0100

    Add basic routes / and /catalog

--- views.py
+++ views.py
@@ -2,6 +2,15 @@ from flask import Flask, jsonify, request, url_for
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+@app.route('/catalog', methods=['GET'])
+def main():
+    return ('Catalog Home page')
+
+@app.route('/login', methods=['GET'])
+def login():
+    return ('Login screen requested')
+
 if __name__ == "__main__":
     app.secret_key='super_secret_key' # Change this to a proper secret key later
     app.debug = True
