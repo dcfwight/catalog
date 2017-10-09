503eeec09f55cf911e215fd5bcba119f560a8d9c
Doug Wight <dcfwight@gmail.com>
2017-10-09 15:33:23 +0100

    Add time and owner_id fields to dummy_data so it can be sorted

--- views.py
+++ views.py
@@ -4,22 +4,36 @@ import pprint as pp
 app = Flask(__name__)
 
 dummy_data = {'soccer':{
-                'balls': 'round things that you can kick',
-                'nets': 'stringy things that catch balls'},
-              'rugby': {
-                'balls': 'oval things that you can carry OR kick',
-                'boots': 'heavy, leathery things you wear on your feet'
-              },
-              'snowboarding': {
-                'goggles': 'protective screen for your eyes',
-                'snowboard': 'Best for any terrain and condition. All-mountain snowboards blah '
-              }
-              }
+                'balls': {'desc':'round things that you can kick',
+                          'time': 5,
+                          'owner_id': 2},
+                'nets': {'desc': 'stringy things that catch balls',
+                         'time': 3,
+                         'owner_id': 3}
+                        },
+            'rugby': {
+                'balls': {'desc': 'oval things that you can carry OR kick',
+                          'time': 1,
+                          'owner_id': 1},
+                'boots': {'desc': 'heavy, leathery things you wear on your feet',
+                          'time': 7,
+                          'owner_id': 2}
+                    },
+            'snowboarding': {
+                'goggles': {'desc': 'protective screen for your eyes',
+                            'time': 2,
+                            'owner_id': 1},
+                'snowboard': {'desc': 'Best for any terrain and condition. All-mountain snowboards blah ',
+                              'time' : 4,
+                              'owner_id': 3}
+                    }
+            }
 
 @app.route('/', methods=['GET'])
 @app.route('/catalog/', methods=['GET'])
 def index():
     return render_template('catalog.html', data=dummy_data)
+
 
 @app.route('/login/', methods=['GET'])
 def login():
@@ -27,7 +41,10 @@ def login():
 
 @app.route('/catalog/<string:category>/', methods=['GET'])
 def item(category):
-    return render_template('category.html', category=category)
+    if dummy_data.get(category):
+        return render_template('category.html', category=category, data = dummy_data[category])
+    else:
+        return render_template('catalog.html', data=dummy_data)
 
 
 if __name__ == "__main__":
