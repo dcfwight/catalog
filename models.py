c8d958800fb3cbb544db1b756ec6155913fbe84e
Doug Wight <dcfwight@gmail.com>
2017-10-09 22:12:35 +0100

    CRUD - reading categories from database

--- models.py
+++ models.py
@@ -1,27 +1,72 @@
-from sqlalchemy import Column,Integer,String
+# START of configration code
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
-# We are now adding in a password hashing module - there are many - we are using passlib and custom_app_context
-from passlib.apps import custom_app_context as pwd_context
+from passlib.apps import custom_app_context as pwd_context # password hashing
 
 Base = declarative_base()
+# END of configuration code
+
 class User(Base):
-    __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
-    username = Column(String(32), index=True)
-    password_hash = Column(String(64))
-    
-    # hashing is a one-way process - we store the hashed password, and NEVER the actual password.
-    def hash_password(self, password):
-        self.password_hash = pwd_context.encrypt(password)
-    
-    # So, to verify it, we take the offered password, run the hashing process again, and check against the hashed password which is stored.
-    def verify_password(self, password):
-        return pwd_context.verify(password, self.password_hash)
+	__tablename__ = 'user'
+	id = Column(Integer, primary_key=True)
+	username = Column(String(32), index=True)
+	password_hash = Column(String(64))
+
+	# hashing is a one-way process - we store the hashed password, and NEVER the actual password.
+	def hash_password(self, password):
+		self.password_hash = pwd_context.encrypt(password)
+
+	# So, to verify it, we take the offered password, run the hashing process again, and check against the hashed password which is stored.
+	def verify_password(self, password):
+		return pwd_context.verify(password, self.password_hash)
+	
+	@property
+	def serialize(self):
+		"""Return object data in easily serializeable format"""
+		return {
+			'id': self.id,
+			'username' : self.name,
+			'password_has': self.password_hash
+			}
+
+class Category(Base):
+	__tablename__ = 'category'
+	id = Column(Integer, primary_key=True)
+	name = Column(String(32), nullable=False, index=True)
+
+	@property
+	def serialize(self):
+		"""Return object data in easily serializeable format"""
+		return {
+		'id': self.id,
+		'name' : self.name
+		}
+
+class Item(Base):
+	__tablename__ = 'item'
+	id = Column(Integer, primary_key=True)
+	name = Column(String(32), index=True)
+	description = Column(String(1024))
+	category_id = Column(Integer, ForeignKey('category.id'))
+	category = relationship(Category)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
+
+	@property
+	def serialize(self):
+		"""Return object data in easily serializeable format"""
+		return {
+			'id': self.id,
+			'name' : self.name,
+			'description': self.description
+			}
 
 
-engine = create_engine('sqlite:///users.db')
- 
+
+engine = create_engine('sqlite:///catalog.db')
+# this is pointed to the database we will create and use - NOTE the three backslashes
 
 Base.metadata.create_all(engine)
+# goes into the database and adds the classes we will create as new tables in the database.
--- views.py
+++ views.py
@@ -1,38 +1,64 @@
 from flask import Flask, jsonify, render_template, request, url_for
-import pprint as pp
+import pprint as pp # for pretty-printing
+# from collections import OrderedDict # to enable sorting on nested dictionaries
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, User, Category, Item
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 app = Flask(__name__)
 
-dummy_data = {'soccer':{
+dummy_categories = {'soccer':{'id':1},
+                    'rugby':{'id':2},
+                    'snowboarding':{'id':3}
+                    }
+
+dummy_items = {
                 'balls': {'desc':'round things that you can kick',
                           'time': 5,
-                          'owner_id': 2},
+                          'owner_id': 2,
+                          'category_id': 1},
                 'nets': {'desc': 'stringy things that catch balls',
                          'time': 3,
-                         'owner_id': 3}
-                        },
-            'rugby': {
+                         'owner_id': 3,
+                         'category_id': 1},
+                        
+            
                 'balls': {'desc': 'oval things that you can carry OR kick',
                           'time': 1,
-                          'owner_id': 1},
+                          'owner_id': 1,
+                          'category_id': 2},
                 'boots': {'desc': 'heavy, leathery things you wear on your feet',
                           'time': 7,
-                          'owner_id': 2}
-                    },
-            'snowboarding': {
+                          'owner_id': 2,
+                          'category_id': 2},
+                    
                 'goggles': {'desc': 'protective screen for your eyes',
                             'time': 2,
-                            'owner_id': 1},
+                            'owner_id': 1,
+                            'category_id': 3},
                 'snowboard': {'desc': 'Best for any terrain and condition. All-mountain snowboards blah ',
                               'time' : 4,
-                              'owner_id': 3}
-                    }
+                              'owner_id': 3,
+                              'category_id': 3}
+                   
             }
 
+    
+
 @app.route('/', methods=['GET'])
-@app.route('/catalog/', methods=['GET'])
+@app.route('/catalog/', methods=['GET'])    
 def index():
-    return render_template('catalog.html', data=dummy_data)
+    categories = session.query(Category).order_by(Category.name)
+    for category in categories:
+        pp.pprint(category.serialize)
+    return render_template('catalog.html', categories=categories)
 
 
 @app.route('/login/', methods=['GET'])
@@ -41,7 +67,7 @@ def login():
 
 @app.route('/catalog/<string:category>/', methods=['GET'])
 def item(category):
-    if dummy_data.get(category):
+    if category in dummy_categories:
         return render_template('category.html', category=category, data = dummy_data[category])
     else:
         return render_template('catalog.html', data=dummy_data)
