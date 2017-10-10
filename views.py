dedb8da1055a3d949ebc2271548463f73042b71b
Doug Wight <dcfwight@gmail.com>
2017-10-10 15:36:28 +0100

    Add category_display method to retrieve category info from db

--- views.py
+++ views.py
@@ -15,52 +15,6 @@ session = DBSession()
 
 app = Flask(__name__)
 
-dummy_categories = {
-	'soccer':{'id':1},
-	'rugby':{'id':2},
-	'snowboarding':{'id':3}
-}
-
-dummy_items = {
-	'balls': {
-		'desc':'round things that you can kick',
-		'time': 5,
-		'owner_id': 2,
-		'category_id': 1
-	},
-	'nets': {
-		'desc': 'stringy things that catch balls',
-		'time': 3,
-		'owner_id': 3,
-		'category_id': 1
-	},
-	'balls': {
-		'desc': 'oval things that you can carry OR kick',
-		'time': 1,
-		'owner_id': 1,
-		'category_id': 2
-	},
-	'boots': {
-		'desc': 'heavy, leathery things you wear on your feet',
-		'time': 7,
-		'owner_id': 2,
-		'category_id': 2
-	},
-	'goggles': {
-		'desc': 'protective screen for your eyes',
-		'time': 2,
-		'owner_id': 1,
-		'category_id': 3
-	},
-	'snowboard': {
-		'desc': 'Best for any terrain and condition. All-mountain snowboards blah ',
-		'time' : 4,
-		'owner_id': 3,
-		'category_id': 3
-	}
-}
-
-
 @app.route('/', methods=['GET'])
 @app.route('/catalog/', methods=['GET'])
 def showCatalog():
@@ -83,6 +37,7 @@ def createItem(name, description, category_id, user_id):
 
 @app.route('/editItem/<int:id>', methods=['POST'])	
 def editItem(id):
+	edit_time = int(time.time())
 	if request.method=='POST':
 		item_to_edit = session.query(Item).filter_by(id = id).one()
 		print ('item to edit retrieved from database')
@@ -92,11 +47,13 @@ def editItem(id):
 		if request.form['description']:
 			item_to_edit.description = request.form['description']
 		if request.form['category_id']:
-			item_to_edit.description = request.form['category_id']
+			item_to_edit.category_id = request.form['category_id']
 		if request.form['user_id']:
-			item_to_edit.description = request.form['user_id']
+			item_to_edit.user_id = request.form['user_id']
 		for key in request.form:
 			print(key, ': ', request.form[key] )
+		if request.form != []:
+			item_to_edit.create_time = edit_time
 		session.add(item_to_edit)
 		session.commit()
 		return ('item with id: {} has been edited'.format(item_to_edit.id))
@@ -106,12 +63,19 @@ def login():
 	return render_template('login.html')
 
 @app.route('/catalog/<string:category>/', methods=['GET'])
-def item(category):
-	if category in dummy_categories:
-		return render_template('category.html', category=category,
-			data = dummy_data[category])
-	else:
-		return render_template('catalog.html', data=dummy_data)
+def category_display(category):
+	print ('category from GET request is {}'.format(category))
+	category_to_show = session.query(Category).filter_by(name = category).first()
+	pp.pprint (category_to_show.serialize)
+	items_to_show = session.query(Item)\
+		.filter_by(category_id = category_to_show.id)\
+		.order_by(Item.name).\
+		all()
+	for item in items_to_show:
+		pp.pprint (item.serialize)
+	return render_template('category.html', category = category_to_show,
+						   items = items_to_show)
+	# return jsonify(category_to_show.serialize)
 
 if __name__ == "__main__":
 	# createItem('goggles','protective eyewear', 3, 2)
