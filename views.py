b8792ed682de9cc2a3d875365fe96d23040499c4
Doug Wight <dcfwight@gmail.com>
2017-10-12 10:31:48 +0100

    Add fbconnect and fbdisconnect

--- views.py
+++ views.py
@@ -329,6 +329,92 @@ def gdisconnect():
 		response.headers['Content-Type']= 'application/json'
 		return response
 
+################################################################################
+## FACEBOOK connection ##
+
+@app.route('/fbconnect', methods = ['POST'])
+def fbconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = request.data
+    
+    # Exchange client token for long-lived server-side token with GET/oauth/
+    # access_token?grant_type=fb_exchange_token&client_id={app-id}
+    # &client_secret={app-secret}&fb_exchange_token={short-lived_token}
+    app_id = json.loads(open('fb_client_secrets.json','r').read())['web']['app_id']
+    app_secret = json.loads(open('fb_client_secrets.json','r').read())['web']['app_secret']
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' %(app_id, app_secret, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    print ('result is: ')
+    print (result)
+    data =json.loads(result)
+    token = 'access_token='+data['access_token']
+    print ('token is: %s' %token)
+    
+    #use token to get user info from API
+    userinfo_url = "https://graph.facebook.com/v2.2/me"
+    # strip expire tag from access token
+    #token = result.split('&')[0]
+    
+    url = 'https://graph.facebook.com/v2.8/me?%s&fields=name,id,email' % token
+    h = httplib2.Http()
+    result =h.request(url, 'GET')[1]
+    print ('url sent for API access: %s' %url)
+    print ("API JSON result: %s" %result)
+    data = json.loads(result)
+    login_session['provider'] = 'facebook'
+    login_session['username'] = data["name"]
+    login_session['email'] = data['email']
+    login_session['facebook_id'] = data["id"]
+    
+    # Get user picture
+    # facebook uses a seperate API call to get user picture
+    url = 'https://graph.facebook.com/v2.2/me/' \
+          'picture?%s&redirect=0&height=200&width=200' % token
+    h = httplib2.Http()
+    result =h.request(url, 'GET')[1]
+    print ('result from user picture request is: ')
+    print (result)
+    data = json.loads(result)
+    login_session['picture'] = data['data']['url']
+    
+    # see if user exists in our database. If it doesn't, make a new one
+    userId=getUserID(login_session['email'])
+    if not userId:
+        createUser(login_session)
+    login_session['user_id'] = userId
+
+    print(login_session)
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += '"style="width: 300px; height: 300px; border-\
+        radius: 150px;-webkit-border-radius: 150px;-moz-border-radius:150px;">'
+    flash("you are now logged in as %s"%login_session['username'])
+    print (output)
+    return output
+
+@app.route('/fbdisconnect')
+# Note that this is now supplemented by the generic /disconnect function
+def fbdisconnect():
+    facebook_id = login_session['facebook_id']
+    url = 'https://graph.facebook.com/%s/permissions' % facebook_id
+    h = httplib2.Http()
+    result = h.request(url, 'DELETE') [1]
+    # the deletions used to be coded here, but are now done in the generic /disconnect function
+    # del login_session['username']
+    # del login_session['email']
+    # del login_session['picture']
+    # del login_session['user_id']
+    # del login_session['facebook_id']
+    return "you have been logged out"
+
 def getUserID(email):
     try:
         user = session.query(User).filter_by(email = email).one()
