73fddbcc06be556b358bb7f75797d2f26c131c0f
Doug Wight <dcfwight@gmail.com>
2017-10-24 09:52:22 +0100

    Move Forms code to separate forms.py file

--- views.py
+++ views.py
@@ -6,13 +6,7 @@ from flask import session as login_session
 from flask_bootstrap import Bootstrap
 # flask Boostrap extension to help with implementing Bootstrap.
 
-from flask_wtf import FlaskForm
-# Form class - allows for creation in Python, then rendered. Better validation
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-class NameForm(FlaskForm):
-    name = StringField('Name: ', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+from forms import NameForm
 
 import random, string
 import pprint
