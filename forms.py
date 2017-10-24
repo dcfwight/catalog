53d8aa6f982cc9e9be6d2652475260f135669c2d
Doug Wight <dcfwight@gmail.com>
2017-10-24 10:22:26 +0100

    Replace DataRequired with InputRequired (recommended)

--- forms.py
+++ forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
 # Form class - allows for creation in Python, then rendered. Better validation
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired, Length
 
 class NameForm(FlaskForm):
-    name = StringField('Name: ', validators=[DataRequired()])
+    name = StringField('Name: ', validators=[InputRequired(),
+                                             Length(max=32)])
     submit = SubmitField('Submit')
