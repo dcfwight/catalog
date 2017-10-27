5075986026eab74643459ff8e6198894b3b97be6
Doug Wight <dcfwight@gmail.com>
2017-10-27 09:06:23 +0100

    Add comments

--- forms.py
+++ forms.py
@@ -4,6 +4,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import InputRequired, Length
 
 class NameForm(FlaskForm):
-    name = StringField('Name: ', validators=[InputRequired(),
-                                             Length(max=32)])
+    name = StringField('Name: ', validators=[InputRequired(), Length(max=32)])
     submit = SubmitField('Submit')
+    
+# Note - can add many more forms, e.g. email forms (better verification)
