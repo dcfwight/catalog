ab7344a4ac94f31bddbb119e4608d6f07fa4253a
Doug Wight <dcfwight@gmail.com>
2017-12-13 16:31:53 +0000

    Revert changes so master works again

--- app/main/routes/dummy.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request
-
-dummy = Blueprint('dummy',__name__)
-
-@dummy.route('/test', methods=['GET','POST'])
-def dummy_test():
-	if request.method =='GET':
-		return'dummy/test GET received via Blueprint!'
