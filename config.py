d667391c9cde4223ea7f359115b08dc28951f254
Doug Wight <dcfwight@gmail.com>
2017-12-18 09:41:27 +0000

    Removed old file structure

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__name__))
+
+USER = 'dougwight'
+PASSWORD = 'Linton'
+HOST = 'localhost'
+PORT = 5432 # default port for postgresql
+DATABASE = 'catalog'
+
+class Config:
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string camel176'
+	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+	
+	@staticmethod
+	def init_app(app):
+		pass
+
+class DevelopmentConfig(Config):
+	DEBUG = True
+	POSTGRESQL_DATABASE_URI = os.environ.get('POSTGRESQL_DATABASE_URI') or \
+		'postgresql://{}:{}@{}:{}/{}'.format(USER,PASSWORD,HOST,PORT,DATABASE)
+	
+config = {
+	'development': DevelopmentConfig,
+	'default': DevelopmentConfig
+}
