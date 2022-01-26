5f629dc95a92068353d341befe10d1ee36f457f1
Doug Wight <dcfwight@gmail.com>
2022-01-26 16:25:52 +0000

    Tidy up typos

--- manage.py
+++ manage.py
@@ -10,15 +10,18 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 # Having to import the database instance and models each time a shell session is
-# started is tedious work. To avoid having to constanly repeat these imports, the
-# Flask-Script's shell cmmmand can be configured to import certain objects
-# To add objects tot he import list, the shell command needs to be registered
+# started is tedious work. To avoid having to constantly repeat these imports, the
+# Flask-Script's shell command can be configured to import certain objects
+# To add objects to the import list, the shell command needs to be registered
 # with a make_context callback function
 
 # To run this, enter python manage.py shell at command line
 
+
 def make_shell_context():
 	return dict(app=app, db=db, User=User, Category=Category, Item=Item)
+
+
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
